3107a0c52089ab31af2fe07cd0232b90b373f563
Manoj kumar Lekkalapudi <manojkumarlekkalapudi@Manojs-MacBook-Pro.local>
2018-03-26 21:11:36 -0400

    first commit

---
+++ src/main/java/com/syntel/rest/webservices/springbootrestfulservices/SpringbootRestfulServicesApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.rest.webservices.springbootrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulServicesApplication.class, args);
+	}
+}
